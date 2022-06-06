81ebd93e02adf954675369bb9796b26def4dbc37
grace <chanheeya@gmail.com>
2022-06-06 18:26:35 -0500

    2nd frame with ping

--- app/helpers/hotwire_helper.rb
+++ app/helpers/hotwire_helper.rb
@@ -1,2 +1,13 @@
 module HotwireHelper
+  def random_color
+    "%06x" % (rand * 0xffffff)
+  end
+
+  def pinged
+    Time.now.to_formatted_s(:db)
+  end
+
+  def pinged_to_mil
+    Time.now.to_f
+  end
 end
